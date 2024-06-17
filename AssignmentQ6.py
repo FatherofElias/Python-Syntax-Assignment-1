395ad0aff6f6592ad843b40a53f2d6635dc53ad8
FatherofElias <supra_3_8@hotmail.com>
2024-06-17 11:10:10 -0500

    update Q6

---
+++ AssignmentQ6.py
@@ -0,0 +1,24 @@
+#Task 1
+
+normal_expression = 3 + 4 * 8
+print(normal_expression)
+
+parenthesis_expression = (3 + 4) * 8
+
+print(parenthesis_expression)
+
+if normal_expression == parenthesis_expression:
+    print("Same")
+else:
+    normal_expression != parenthesis_expression
+    print("Different")
+
+#Task 2
+
+a = 20
+b = 40
+c = 55
+
+result = a < b < c 
+
+print("Is b greater than a and less than c?", result)
