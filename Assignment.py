fd945575c22dfb6536826a83fca3831348fc320c
FatherofElias <supra_3_8@hotmail.com>
2024-06-16 20:07:02 -0500

    update

--- Assignment.py
+++ Assignment.py
@@ -5,3 +5,10 @@ if weather == "sunny":
     print("Wear sunglasses!")
 else:
     print("Take an umbrella!")
+
+#Task 2
+user = "sad"
+print("How are you feeling today?")
+if user == "happy": print("That's great to hear!")
+else:
+    if user == "sad": print("I hope your day gets better!")
